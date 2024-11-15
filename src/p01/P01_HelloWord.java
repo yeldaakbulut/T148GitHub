cb3d0b98a72f656efd364f16c68c48eca5953d8a
yelda <yeldakbulut@gmail.com>
2024-11-15 22:24:36 +0100

    second commit

--- src/p01/P01_HelloWord.java
+++ src/p01/P01_HelloWord.java
@@ -5,5 +5,7 @@ public class P01_HelloWord {
     public static void main(String[] args) {
 
         System.out.println("Hello Word");
+
+        System.out.println("Hello Github");
     }
 }
--- src/p02/P01.java
+++ src/p02/P01.java
@@ -1,5 +1,7 @@
 package p02;
 
+import java.util.Scanner;
+
 public class P01 {
     public static void main(String[] args) {
 
@@ -7,5 +9,18 @@ public class P01 {
         int b=17;
 
         System.out.println(a+b);
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Bir sayi giriniz");
+        int sayi= scanner.nextInt();
+
+        if (sayi %2==0){
+            System.out.println("Girdiginiz sayi bir cift sayidir");
+        }else {
+            System.out.println("Girdiginiz sayi tektir");
+        }
+
+
     }
+
 }
